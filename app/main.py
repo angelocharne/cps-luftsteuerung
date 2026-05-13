71d5a7511731a902b197cdf1055e69dd627070f8
Cargnx <117575681+Altaiir1@users.noreply.github.com>
2026-05-13 15:09:02 +0200

    [WIP] Test main.py with sensor checking

---
+++ app/main.py
@@ -0,0 +1,47 @@
+import os
+import time
+import RPi.GPIO as GPIO
+import board
+import busio
+import adafruit_bme280.basic as adafruit_bme280
+from influxdb_client import InfluxDBClient, Point
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+SENSOR_ID       = os.getenv("SENSOR_ID", "1")
+SENSOR_NAME     = os.getenv("SENSOR_NAME", "BME280")
+SENSOR_LOCATION = os.getenv("SENSOR_LOCATION", "Serverraum")
+INTERVAL        = int(os.getenv("SENSOR_INTERVAL", "10"))
+
+INFLUX_URL    = os.getenv("INFLUXDB_URL", "http://influxdb:8086")
+INFLUX_TOKEN  = os.getenv("DOCKER_INFLUXDB_INIT_ADMIN_TOKEN")
+INFLUX_ORG    = os.getenv("DOCKER_INFLUXDB_INIT_ORG")
+INFLUX_BUCKET = os.getenv("DOCKER_INFLUXDB_INIT_BUCKET")
+
+GPIO.setmode(GPIO.BCM)
+
+i2c = busio.I2C(board.SCL, board.SDA)
+sensor = adafruit_bme280.Adafruit_BME280_I2C(i2c)
+
+client = InfluxDBClient(url=INFLUX_URL, token=INFLUX_TOKEN, org=INFLUX_ORG)
+write_api = client.write_api(write_options=SYNCHRONOUS)
+
+print(f"Sensor '{SENSOR_NAME}' ({SENSOR_ID}) @ {SENSOR_LOCATION} gestartet.")
+
+try:
+    while True:
+        temperature = sensor.temperature
+
+        point = (
+            Point("temperature_reading")
+            .tag("sensor_id", SENSOR_ID)
+            .tag("name", SENSOR_NAME)
+            .tag("location", SENSOR_LOCATION)
+            .field("temperature", temperature)
+        )
+
+        write_api.write(bucket=INFLUX_BUCKET, record=point)
+        print(f"{SENSOR_NAME} @ {SENSOR_LOCATION}: {temperature:.2f}°C")
+
+        time.sleep(INTERVAL)
+finally:
+    GPIO.cleanup()
