f8a0dea34740d34cd1484b77361b290af0d8164e
Angelo <72870190+angelocharne@users.noreply.github.com>
2026-05-20 16:05:26 +0200

    [TASK] Add new route

--- api/main.py
+++ api/main.py
@@ -1,6 +1,45 @@
-from fastapi import FastAPI
+import json
+from pathlib import Path
+from fastapi import FastAPI, HTTPException
+
+import json
+from pathlib import Path
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
+
+DATA_FILE = Path("/app/data/sensor_data.json")
+
+@app.get("/sensor-data")
+def sensor_data():
+    try:
+        with open(DATA_FILE, "r", encoding="utf-8") as file:
+            data = json.load(file)
+
+        if not data:
+            raise HTTPException(
+                status_code=404,
+                detail="Keine Sensordaten vorhanden"
+            )
+
+        # letzten Eintrag zurückgeben
+        latest_sensor_data = data[-1]
+
+        return latest_sensor_data
+
+    except FileNotFoundError:
+        raise HTTPException(
+            status_code=404,
+            detail="JSON-Datei nicht gefunden"
+        )
+
+    except json.JSONDecodeError:
+        raise HTTPException(
+            status_code=500,
+            detail="Fehlerhafte JSON-Datei"
+        )
+
+
 
 @app.get("/")
 def home():
@@ -9,7 +48,3 @@ def home():
 @app.get("/health")
 def health():
     return {"ok": True}
-
-@app.get("/temperature")
-def health():
-    return {"temperature": "20 Grad"}
